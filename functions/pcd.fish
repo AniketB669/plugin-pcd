ad576bbfb797b2f055d5bb0f307e9449d4d89fe8
Aniket Bhattacharyea <aniketmail669@gmail.com>
2019-01-30 23:44:20 +0530

    Use source

--- functions/pcd.fish
+++ functions/pcd.fish
@@ -1,12 +1,12 @@
 function pcd -d "Run script when cd'ing in or out of a directory"
   if test -e '.pcd-exit'
-  	. ".pcd-exit"
+  	source ".pcd-exit"
   else
   	echo ".pcd-exit not found. Skipping."
   end
   cd $argv[1]
   if test -e ".pcd-enter"
-  	. ".pcd-enter"
+  	source ".pcd-enter"
   else
   	echo ".pcd-enter not found. Skipping"
   end
